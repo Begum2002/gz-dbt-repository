38e671a1d75fda3059b4e56797ee429c66b4c6c6
Begum Yapicioglu <begum.yapicioglu@hotmail.com>
2025-11-08 11:35:31 +0000

    sql dosyalarini duzenledik

---
+++ models/stages/stg_raw__facebook.sql
@@ -0,0 +1,17 @@
+WITH source AS (
+    SELECT * FROM {{ source('raw', 'facebook') }}
+),
+
+renamed AS (
+    SELECT
+        date_date,
+        paid_source,
+        campaign_key,
+        camPGN_name AS campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        impression AS ads_impression,
+        click AS ads_clicks
+    FROM source
+)
+
+SELECT * FROM renamed
